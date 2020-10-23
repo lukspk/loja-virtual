2bd7b5e30a3899b9effc851775090e055b61930c
Lucas <g.lucas.silva@gmail.com>
2020-10-23 09:46:42 -0400

    Alteracao host
    Alteracao host

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,30 +1,8 @@
-// {
-//   "development": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_development",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "test": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_test",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "production": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_production",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   }
-// }
+
 module.exports = {
   username: 'default',
   password: 'secret',
   database: 'loja',
-  host: 'loja-postgres',
+  host: '127.0.0.1',
   dialect: 'postgres',
 }
